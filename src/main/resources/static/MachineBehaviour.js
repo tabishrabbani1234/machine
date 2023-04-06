bee97841cd1a702b0bdbad8c194c3dd06812e238
Tabish Rabbani <lone_wolf@Tabishs-MacBook-Air.local>
2023-04-07 00:56:36 +0530

    Final Solution

--- src/main/resources/static/MachineBehaviour.js
+++ src/main/resources/static/MachineBehaviour.js
@@ -1,10 +1,13 @@
 var eventType;
-var machineState;
+var machineState = "Initial";
 var eventTimeStamp = 0;
 var machineRowIndex = 10;
 var machineColIndex = 30;
 var machineTimeStamp = 0;
+var eventType = "Initial";
 var eventList = [];
+var onLoadTime = 0;
+const date = new Date()
 function upArrowClick(){
    eventType = "Move Up";
    eventList.push(eventType);
@@ -34,30 +37,29 @@ function downArrowClick(){
    requestHandlerFunction();
 }
 function getTimeStamp(event) {
-  eventTimeStamp = Math.floor(event.timeStamp);
+  eventTimeStamp = Math.floor(Date.now()) - onLoadTime;
 }
 
 function addGridElementStyle() {
-    var count = ((machineRowIndex-1) * 60) + machineColIndex ;
+    var count = ((parseInt(machineRowIndex)-1) * 60) + parseInt(machineColIndex) ;
     var cell = ("div").concat(count.toString());
     var div = document.getElementById(cell);
-    div.focus();
     div.style.background = "green";
     if(machineState == "Focusing"){
-       div.style.border-style = "dashed";
+       div.style.border = "thin dotted #000000";
     } else if(machineState == "Capturing"){
-       div.style.border-style = "solid";
+       div.style.border = "thick solid #000000";
     } else {
-       div.style.border-style = "none";
+       div.style.border = "none";
     }
 }
 
 function removeGridElementStyle() {
-    var count = ((machineRowIndex-1) * 60) + machineColIndex ;
+    var count = ((parseInt(machineRowIndex)-1) * 60) + parseInt(machineColIndex) ;
     var cell = ("div").concat(count.toString());
     var div = document.getElementById(cell);
     div.style.background = "white";
-    div.style.border-style = "none";
+    div.style.border = "none"
 }
 
 function createGrid(){
@@ -80,48 +82,73 @@ function createGrid(){
      elem.parentNode.removeChild(elem);
      elem = document.getElementById('Machine Header');
      elem.parentNode.removeChild(elem);
+     machineTimeStamp = eventTimeStamp;
+     onLoadTime = Date.now();
      addGridElementStyle();
      requestHandlerFunction();
 }
 
 function requestHandlerFunction(){
+      eventTimeStamp -= (eventTimeStamp%100);
       const xhr = new XMLHttpRequest();
+      var jsonArray = eventList.toString();
       var url = ("http://localhost:8080/morphleLabs");
-      var jsonArray = JSON.stringify(eventList);
       xhr.open("POST", url);
+      xhr.setRequestHeader("Accept", "application/json");
       xhr.setRequestHeader("Content-Type", "application/json;charset=UTF-8");
-      xhr.send({
-           data: {"eventTimeStamp": eventTimeStamp, "eventType": eventType, "eventList": jsonArray, "machineRowIndex": machineRowIndex, "machineColIndex": machineColIndex, "machineTimeStamp":machineTimeStamp, "machineState": machineState}
-      });
+      xhr.setRequestHeader("eventListString", jsonArray);
+      const machineDetails = {
+          "eventTimeStamp": eventTimeStamp,
+          "machineState": machineState,
+          "machineRowIndex": machineRowIndex,
+          "machineColIndex": machineColIndex,
+          "machineTimeStamp":machineTimeStamp,
+          "machineState": machineState
+          };
+          console.log(machineDetails);
+      var data = JSON.stringify(machineDetails);
+      xhr.send(data);
       xhr.onload = () => {
         if (xhr.status == 200) {
-          const timeStamp = xhr.getResponseHeader("eventTime");
+          const timeStamp = xhr.getResponseHeader("machineTimeStamp");
           const state = xhr.getResponseHeader("machineState");
           const x_pos = xhr.getResponseHeader("rowIndex");
           const y_pos = xhr.getResponseHeader("colIndex");
           const eventListStatus = xhr.getResponseHeader("eventListStatus");
-          responseHandlerFunction(timeStamp, state, x_pos, y_pos);
+          responseHandlerFunction(timeStamp, state, x_pos, y_pos, eventListStatus);
         } else {
           console.log(`Error: ${xhr.status}`);
         }
       };
 }
 //Response Handler functions
-function responseHandlerFunction(var timeStamp, var state, var x_pos, var y_pos){
-     if(timeStamp !== machineTimeStamp){
+function responseHandlerFunction(timeStamp, state, x_pos, y_pos, eventListStatus){
+     if(parseInt(timeStamp) != machineTimeStamp){
          removeGridElementStyle();
-         machineTimeStamp = timeStamp;
-         machineRowIndex = x_pos;
-         machineColIndex = y_pos;
+         machineTimeStamp = parseInt(timeStamp);
+         machineRowIndex = parseInt(x_pos);
+         machineColIndex = parseInt(y_pos);
          machineState = state;
          addGridElementStyle();
+         if(eventListStatus == "clear")
+            eventList.length = 0;
+         setMachineRequestcall(machineTimeStamp);
      } else {
-         setMachineRequestcall(machine.timeStamp);
+        if(state = "Idle"){
+            machineState = state;
+            addGridElementStyle();
+        }
      }
 }
 
-function setMachineRequestcall(var time){
-   var currentTime = new Date();
-   var timeInMilliSeconds = time - currentTime.getMilliseconds();
-   window.setTimeout(requestHandlerFunction, timeInMilliSeconds);
+function updateEventTimeStamp(){
+     eventTimeStamp = machineTimeStamp;
+     requestHandlerFunction();
+}
+
+function setMachineRequestcall(time){
+   var currentTime = Date.now() - onLoadTime;
+   var timeInMilliSeconds = time - currentTime;
+   //console.log(currentTime, " ", time);
+   window.setTimeout(updateEventTimeStamp, timeInMilliSeconds);
 }
